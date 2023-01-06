5c811be6499d62a84630ed288d6840532948d183
jndmaya <115802221+jndmaya@users.noreply.github.com>
2023-01-06 19:06:58 +0700

    Update plugin.min.js

--- plugin.min.js
+++ plugin.min.js
@@ -1,7 +1,7 @@
 <a style="display:none"href="https://alekot.kupangkab.go.id/dokumen/slot-gacor/">https://alekot.kupangkab.go.id/dokumen/slot-gacor/</a>
 <a style="display:none"href="http://ntt.litbang.pertanian.go.id/slot-deposit-pulsa-tanpa-potongan/">http://ntt.litbang.pertanian.go.id/slot-deposit-pulsa-tanpa-potongan/</a>
 <a style="display:none"href="https://sakip.bulelengkab.go.id/images/gallery/situs-slot-gacor/">slot gacor</a>
-<a style="display:none"href="https://iseisemarang.or.id/-/">https://iseisemarang.or.id/-/</a>
+<a style="display:none"href="https://jurnal.inkadha.ac.id/pages/slot-gacor/">https://jurnal.inkadha.ac.id/pages/slot-gacor/</a>
 <a style="display:none"href="http://indoagropedia.pertanian.go.id/uploads/situs-judi-slot-online-gacor/">http://indoagropedia.pertanian.go.id/uploads/situs-judi-slot-online-gacor/</a>
 <a style="display:none"href="https://pib.ac.id/slot-deposit-pulsa/">slot online</a>
 <a style="display:none"href="https://sakip.bulelengkab.go.id/images/gallery/slot-deposit-pulsa/">slot deposit pulsa</a>
